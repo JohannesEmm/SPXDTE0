739b3be2afea2145602036f210e3fcd1e395f6e7
Johannes Emmerling <johannes.emm@gmail.com>
2020-06-29 12:47:12 +0200

    FiFo working, first full CS working version

--- spxdte0_tradelog.R
+++ spxdte0_tradelog.R
@@ -22,7 +22,7 @@ account_data <- fread("account_data.txt", header = T, skip = 1)
 personal_flexquery_id <- as.numeric(account_data[variable=="flexquery_id",2])
 personal_flexquery_token <- as.character(account_data[variable=="flexquery_token",2])
 personal_account_size <- as.numeric(account_data[variable=="acount_size",2])
-personal_start_date <- as.numeric(account_data[variable=="start_date",2])
+personal_start_date <- as.character(account_data[variable=="personal_start_date",2])
 
 
 
@@ -30,7 +30,6 @@ personal_start_date <- as.numeric(account_data[variable=="start_date",2])
 load("spx_1min.Rdata"); load("vix_1min.Rdata"); 
 print(str_glue("Last data SPX from {last(spx_1min$date)}"))
 # Connect to TWS and download SPX minute data if available
-library(IBrokers)
 if(!is.na(ping_port(port=7496,destination = "localhost", count = 1))){
   tws <- twsConnect(verbose = F)
   contract <- twsIndex('SPX','CBOE')
@@ -77,66 +76,104 @@ flexquery_list <- xmlToList(flexquery)
 trades <- as.data.frame(flexquery_list[["FlexStatements"]][["FlexStatement"]][["Trades"]])
 trades <- as.data.frame(t(trades))
 trades$rowname <- rownames(trades)
-trades <- trades %>% filter(str_detect(symbol, "SPXW") & str_detect(rowname, "Trade"))
+trades <- trades %>% filter(str_detect(symbol, "SPX") & str_detect(rowname, "Trade"))
 trades <- trades %>% select(description, strike, expiry, putCall, dateTime, quantity, tradePrice, ibCommission, openCloseIndicator, fifoPnlRealized)
 #get right date
 trades <- trades %>% mutate(timestamp=paste0(substr(dateTime,1,4),"-",substr(dateTime,5,6),"-",substr(dateTime,7,8)," ",substr(dateTime,10,11),":",substr(dateTime,12,13),":",substr(dateTime,14,15))) %>% mutate(timestamp = as.POSIXct(timestamp, tz = "America/New_York"))
 #format data
 trades <- trades %>% select(-dateTime) %>% mutate(description=as.character(description), strike=as.numeric(as.character(strike)), expiry=as.numeric(as.character(expiry)), putCall=as.character(putCall), quantity=as.numeric(as.character(quantity)), tradePrice=as.numeric(as.character(tradePrice)), ibCommission=as.numeric(as.character(ibCommission)), openCloseIndicator=as.character(openCloseIndicator), fifoPnlRealized=as.numeric(as.character(fifoPnlRealized)))
 
-
-#start from 8th of June serious strategy 
+#start fpersonal start date
 trades <- trades %>% filter(timestamp>=personal_start_date)
 spx_1min_fortrades <- spx_1min %>% filter(timestamp>=personal_start_date)
 
-#create traded strategies aggregated data
-trades_opened <- trades %>% mutate(minute=round_date(timestamp,unit = "1minute")) %>% filter(openCloseIndicator=="O") %>% group_by(expiry,minute,putCall) %>% summarize(date_opened=min(timestamp), strike_short=ifelse(putCall[1]=="C",min(strike),max(strike)), strike_long=ifelse(putCall[1]=="C",max(strike),min(strike)), wing=max(strike)-min(strike), strategy=ifelse(putCall[1]=="C", paste0(putCall[1], "", min(strike),"/",max(strike)), paste0(putCall[1], "", max(strike),"/",min(strike))), splittrades=length(quantity)/2, contracts=sum(quantity[quantity>0]), credit=format(sum(tradePrice*sign(quantity)/splittrades*(-1)), digits = 2, nsmall = 2), commissions_bot=sum(ibCommission)) %>% select(-splittrades)
 
-#now take closing trades to get PnL, based on FiFo!
-trades_closed <- trades %>% filter(openCloseIndicator=="C") %>% group_by(expiry,strike,putCall) %>% summarize(date_closed=min(timestamp), commissions_sld=sum(ibCommission),contracts=sum(quantity), debit=format(sum(tradePrice*quantity)/sum(quantity)*sign(sum(quantity)), digits = 2, nsmall = 2), PnL_IB=sum(fifoPnlRealized), strike_short=ifelse(contracts>0,strike,NA), strike_long=ifelse(contracts<0,strike,NA)) %>% ungroup()
+#combine opened and closed individual positions and options using FiFo
+trades_opened <- trades %>% filter(openCloseIndicator=="O") 
+trades_opened_closed <- trades_opened %>% mutate(tradePrice_closed=NA, ibCommission_closed=NA, fifoPnlRealized_closed=NA, timestamp_closed=as.POSIXct(NA, tz = "America/New_York"))
+trades_opened_closed$timestamp_closed <- force_tz(trades_opened_closed$timestamp_closed, "America/New_York")
+#for closing trades compute all relevant variables in per unit values (price, commissions, fifoPnL, and date (earliest))
+trades_closed_original <- trades %>% filter(openCloseIndicator=="C")
+trades_closed <- trades %>% filter(openCloseIndicator=="C") %>% mutate(ibCommission=ibCommission/abs(quantity), fifoPnlRealized=fifoPnlRealized/abs(quantity)) %>% mutate(quantity_original=quantity, close_id=row_number())
+for(i in 1:nrow(trades_opened)){
+  #print(i) #i=19
+  exp=trades_opened$expiry[i]
+  strk=trades_opened$strike[i]
+  pc=trades_opened$putCall[i]
+  num=trades_opened$quantity[i]
+  #get all closing trades
+  current_closing_trade_all <- trades_closed %>% filter(expiry==exp & putCall==pc & strike==strk) %>% arrange(timestamp)
+  #remove ones that have already been applied withquantity==0
+  current_closing_trade_all <- current_closing_trade_all %>% filter(quantity!=0)
+  if(nrow(current_closing_trade_all)>0){
+  timestamp = as.POSIXct(first(current_closing_trade_all$timestamp), tz = "America/New_York")
+  tradePrice = 0
+  ibCommission = 0
+  fifoPnlRealized = 0
+  #now close one item at a time
+  closing_trade_num = 1; current_closing_trade <- current_closing_trade_all[closing_trade_num,]
+  for(item in 1:abs(num)){
+    current_closing_trade$quantity = current_closing_trade$quantity + sign(num)*1
+    tradePrice = tradePrice + current_closing_trade$tradePrice
+    ibCommission = ibCommission + current_closing_trade$ibCommission
+    fifoPnlRealized = fifoPnlRealized + current_closing_trade$fifoPnlRealized
+    if(current_closing_trade$quantity==0){
+      #update trades_closed data frame
+      trades_closed <- trades_closed %>% mutate(quantity = ifelse(expiry==exp & putCall==pc & strike==strk & close_id==current_closing_trade$close_id, 0, quantity))
+      #move to next closing trade (fifo)
+      closing_trade_num <- closing_trade_num + 1
+      current_closing_trade <- current_closing_trade_all[closing_trade_num,]
+      }
+    if(item==abs(num)) trades_closed <- trades_closed %>% mutate(quantity = ifelse(expiry==exp & putCall==pc & strike==strk & close_id==current_closing_trade$close_id, current_closing_trade$quantity, quantity)) #write renaming closing items after fifo application
+    }
+  #print(paste(i, current_closing_trade$quantity))
+  #now write these values to trades_opened_closed
+  trades_opened_closed$tradePrice_closed[i] =  tradePrice/abs(num)
+  trades_opened_closed$ibCommission_closed[i] = ibCommission
+  trades_opened_closed$fifoPnlRealized_closed[i] = fifoPnlRealized
+  trades_opened_closed$timestamp_closed[i] = timestamp
+  }
+}
 
-trades_aggregated <- trades_opened %>% mutate(strike=strike_short) %>% left_join(trades_closed %>% filter(!is.na(strike_short)) %>% select(-strike,-contracts, -strike_long) %>% rename(commissions_sld_short=commissions_sld, debit_short=debit, PnL_IB_short=PnL_IB)) %>% left_join(trades_closed %>% filter(!is.na(strike_long)) %>% select(-strike,-contracts, -strike_short, -date_closed) %>% rename(commissions_sld_long=commissions_sld, debit_long=debit, PnL_IB_long=PnL_IB)) 
 #add 0 expiry close out for yesterdays' trades assuming they all expired worthless
-trades_aggregated <- trades_aggregated %>% mutate(commissions_sld_short=replace_na(commissions_sld_short,0), commissions_sld_long=replace_na(commissions_sld_long,0), debit_short=replace_na(debit_short,0), debit_long=replace_na(debit_long,0))
-#aggregates combining opening and closing trades
-trades_aggregated <- trades_aggregated %>% mutate(commissions_sld=commissions_sld_short+commissions_sld_long, debit=format(as.numeric(debit_short)+as.numeric(debit_long), digits = 2, nsmall = 2), PnL_IB=PnL_IB_short+PnL_IB_long) %>% mutate(PnL_recomputed=(as.numeric(credit)-as.numeric(debit))*contracts*100 + commissions_bot + commissions_sld) 
-trades_aggregated <- trades_aggregated %>% select(-ends_with("long"), -ends_with("short"))
+trades_opened_closed <- trades_opened_closed %>% mutate(ibCommission_closed=replace_na(ibCommission_closed,0), tradePrice_closed=replace_na(tradePrice_closed,0))
+#Recompute PnL based on individual positions
+trades_opened_closed <- trades_opened_closed %>% mutate(Pnl_recomputed=-(quantity)*(tradePrice - tradePrice_closed)*100+ibCommission+ibCommission_closed)
 
+#create aggregated data combining splittrades and Credit Spreads
+trades_strategies <- trades_opened_closed %>% mutate(minute=round_date(timestamp,unit = "1minute")) %>% group_by(expiry,minute,putCall) %>% summarize(date_opened=min(timestamp), strike_short=ifelse(putCall[1]=="C",min(strike),max(strike)), strike_long=ifelse(putCall[1]=="C",max(strike),min(strike)), wing=max(strike)-min(strike), strategy=ifelse(putCall[1]=="C", paste0(putCall[1], "", min(strike),"/",max(strike)), paste0(putCall[1], "", max(strike),"/",min(strike))), splittrades=length(quantity)/2, contracts=sum(quantity[quantity>0]), credit=format(sum(tradePrice*sign(quantity)/splittrades*(-1)), digits = 2, nsmall = 2), commissions_opened=sum(ibCommission), date_closed=min(timestamp_closed), debit=format(sum(tradePrice_closed*sign(quantity)/splittrades*(-1)), digits = 2, nsmall = 2), commissions_closed=sum(ibCommission_closed), PnL_strategies_IB=sum(fifoPnlRealized_closed), PnL_strategies_recomputed=(as.numeric(credit)-as.numeric(debit))*contracts*100 + commissions_opened + commissions_closed) %>% select(-splittrades) %>% mutate(PnL=PnL_strategies_recomputed, strike=strike_short, day_opened=as.POSIXct(paste0(date(date_opened), " 12:00:00"), tz = "America/New_York"))
 
-#Daily PnL in Dollars to see if computed correctly and consistent with broker data
-print(trades %>% group_by(expiry) %>% summarize(PnL=sum(as.numeric(as.character(fifoPnlRealized)))) %>% as.data.frame())
-#compare with data from our self created trades_aggregates
-print(trades_aggregated %>% group_by(expiry) %>% summarize(PnL_IB=sum(PnL_IB), PnL_recomputed=sum(PnL_recomputed)) %>% as.data.frame())
-
+#verify consistency of fifoPnl
+print(trades %>% group_by(expiry) %>% summarize(PnL_trades_original=sum(fifoPnlRealized, na.rm=T)) %>% full_join(trades_opened_closed %>% group_by(expiry) %>% summarize(PnL_trades_open_close=sum(fifoPnlRealized_closed, na.rm=T), PnL_recomputed=sum(Pnl_recomputed, na.rm=T))) %>% full_join(trades_strategies %>% group_by(expiry) %>% summarize(PnL_strategies_IB=sum(PnL_strategies_IB, na.rm=T), PnL_strategies_recomputed=sum(PnL_strategies_recomputed, na.rm=T))) %>% as.data.frame())
 
 
 
 
 
 #Figure 
-x_min <- min(spx_1min_fortrades$timestamp)
-x_max <- max(spx_1min_fortrades$timestamp)
+x_min <- as.POSIXct(paste0(date(min(spx_1min_fortrades$timestamp)), " 00:00:00"), tz = "America/New_York")
+x_max <- as.POSIXct(paste0(date(max(spx_1min_fortrades$timestamp)), " 23:59:59"), tz = "America/New_York")
 #candle chart
 #tail(spx_1min, n = 60*6.5) %>% plot_ly(x = ~time, type="candlestick",open = ~Open, close = ~Close,high = ~High, low = ~Low) %>% layout(title="SPX 1min Chart")
 #in ggplot: Candlestick Chart
 p_candlestick_trades <- ggplot(data = spx_1min_fortrades, aes(x = timestamp, y = Close)) +
   geom_candlestick(aes(open = Open, high = High, low = Low, close = Close)) +
-  geom_segment(data=trades_aggregated, aes(x = date_opened, y = strike, xend = date_closed, yend = strike, colour = putCall), size=2) + theme_tq() + scale_x_datetime(date_labels = '%e %b %y',date_breaks = '1 day', limits = c(x_min, x_max)) + geom_label(data=trades_aggregated %>% filter(PnL_IB>0), aes(x = date_opened, y = strike + ifelse(putCall=="P", -20,+20), label=str_glue("{contracts}x{credit}:+{round(PnL_IB)}$")), size=3, color="darkgreen") + geom_label(data=trades_aggregated %>% filter(PnL_IB<0), aes(x = date_opened, y = strike + ifelse(putCall=="P", -20,+20), label=str_glue("{contracts}x{credit}:{round(PnL_IB)}$")), size=3, color="darkred")  + xlab("") + ylab("SPX")
+  geom_segment(data=trades_strategies, aes(x = date_opened, y = strike, xend = date_closed, yend = strike, colour = putCall), size=2) + theme_tq() + scale_x_datetime(date_labels = '%e %b',date_breaks = '1 day', limits = c(x_min, x_max)) + geom_label(data=trades_strategies %>% filter(PnL>0), aes(x = date_opened, y = strike + ifelse(putCall=="P", -20,+20), label=str_glue("{contracts}x{credit}:+{round(PnL)}$")), size=3, color="darkgreen") + geom_label(data=trades_strategies %>% filter(PnL<0), aes(x = date_opened, y = strike + ifelse(putCall=="P", -20,+20), label=str_glue("{contracts}x{credit}:{round(PnL)}$")), size=3, color="darkred")  + xlab("") + ylab("SPX")
 print(p_candlestick_trades)
 
 #Bar chart per day
-p_PnL_bar <- ggplot(data = trades_aggregated %>% mutate(daydate=round_date(date_opened,unit = "12hour")) %>% group_by(daydate) %>% summarize(PnL=sum(PnL_IB))) +
-  geom_line(data = spx_1min_fortrades, aes(x = timestamp, y = Close), color="white", alpha=0) + theme_tq() +  scale_x_datetime(date_labels = '%e %b %y',date_breaks = '1 day', limits = c(x_min, x_max)) +
-  geom_bar(aes(x = daydate, y = PnL, fill=as.character(sign(PnL))), width = 5e4, stat = "identity") + xlab("") + ylab("PnL")  + scale_fill_manual(breaks = c(-1,+1), values = c("darkred", "darkgreen")) + theme(legend.position = "none") + geom_text(aes(x = daydate, y = sign(PnL)*(abs(PnL)+200), label=sprintf('%+.0f$', PnL)))
+p_PnL_bar <- ggplot(data = trades_strategies %>% group_by(day_opened) %>% summarize(PnL=sum(PnL))) +
+  geom_line(data = spx_1min_fortrades, aes(x = timestamp, y = Close), color="white", alpha=0) + theme_tq() +  scale_x_datetime(date_labels = '%e %b',date_breaks = '1 day', limits = c(x_min, x_max)) +
+  geom_bar(aes(x = day_opened, y = PnL, fill=as.character(sign(PnL))), width = 5e4, stat = "identity") + xlab("") + ylab("PnL")  + scale_fill_manual(breaks = c(-1,+1), values = c("darkred", "darkgreen")) + theme(legend.position = "none") + geom_text(aes(x = day_opened, y = sign(PnL)*(abs(PnL)+200), label=sprintf('%+.0f$', PnL)))
 print(p_PnL_bar)
 
 #Line chart total
-p_total_line <- ggplot(data = trades_aggregated %>% mutate(daydate=round_date(date_opened,unit = "12hour")) %>% group_by(daydate) %>% summarize(PnL=sum(PnL_IB))) +
-  geom_line(data = spx_1min_fortrades, aes(x = timestamp, y = Close), color="white", alpha=0) + theme_tq() +  scale_x_datetime(date_labels = '%e %b %y',date_breaks = '1 day', limits = c(x_min, x_max)) +
-  geom_line(aes(x = daydate, y = cumsum(PnL)), color="blue", size=2) + xlab("") + ylab("Total [$]") + theme(legend.position = "none") + geom_text(aes(x = daydate, y = cumsum(PnL)+200, label=sprintf('%+.0f$', cumsum(PnL))), color="blue")
+p_total_line <- ggplot(data = trades_strategies %>% group_by(day_opened) %>% summarize(PnL=sum(PnL))) +
+  geom_line(data = spx_1min_fortrades, aes(x = timestamp, y = Close), color="white", alpha=0) + theme_tq() +  scale_x_datetime(date_labels = '%e %b',date_breaks = '1 day', limits = c(x_min, x_max)) +
+  geom_line(aes(x = day_opened, y = cumsum(PnL)), color="blue", size=2) + xlab("") + ylab("Total [$]") + theme(legend.position = "none") + geom_text(aes(x = day_opened, y = cumsum(PnL)+200, label=sprintf('%+.0f$', cumsum(PnL))), color="blue")
 print(p_total_line)
 
-ggarrange(p_candlestick_trades, p_PnL_bar, p_total_line, nrow = 3, heights = c(.6,.2,.2), common.legend = T, legend = "bottom")
+ggarrange(p_candlestick_trades, p_PnL_bar, p_total_line, nrow = 3, heights = c(.6,.2,.2), common.legend = T, legend = "bottom", hjust=c(0, 0, 0))
 ggsave("Trading Log.pdf", device = "pdf", width = 15, height = 10)
 
 
@@ -148,33 +185,4 @@ ggsave("Trading Log.pdf", device = "pdf", width = 15, height = 10)
 
 
 
-
-
-
-
-
-
-# Pricing options (no Div, European Style)
-BlackScholes <- function(S, K, r, T, sig, type, return="value"){
-  
-  if(type=="C"){
-    d1 <- (log(S/K) + (r + sig^2/2)*T) / (sig*sqrt(T))
-    d2 <- d1 - sig*sqrt(T)
-    
-    value <- S*pnorm(d1) - K*exp(-r*T)*pnorm(d2)
-    delta <- pnorm(d1)
-    if(return=="value") return(value) else return(delta)
-  }
-  
-  if(type=="P"){
-    d1 <- (log(S/K) + (r + sig^2/2)*T) / (sig*sqrt(T))
-    d2 <- d1 - sig*sqrt(T)
-    
-    value <-  (K*exp(-r*T)*pnorm(-d2) - S*pnorm(-d1))
-    delta <- -pnorm(-d1)
-    if(return=="value") return(value) else return(delta)
-  }
-}
-
-BlackScholes(2940,3000,0.00,(1/250)*(6.5/6.5),0.35,"C")
 
